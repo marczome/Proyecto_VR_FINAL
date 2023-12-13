f2e128e08b543965af14ca67558d8e57d6e1b4c6
marczome <marczm090102@gmail.com>
2023-12-13 12:47:04 +0100

    Intento movimiento por escena (error)

--- Assets/scripts/topoMovimiento.cs
+++ Assets/scripts/topoMovimiento.cs
@@ -2,17 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class topoMovimiento : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float tiempoSubidaMin = 1f;
+    public float tiempoSubidaMax = 10f;
+    public float tiempoMantenimientoMin = 1f;
+    public float tiempoMantenimientoMax = 5f;
+    public float tiempoBajadaMin = 1f;
+    public float tiempoBajadaMax = 10f;
+
+    public int puntuacion = 0;
+
+    private bool colisiona = false;
+
+    private void Start()
     {
-        
+        // Iniciar el bucle de comportamiento
+        StartCoroutine(CicloSubirBajar());
     }
 
-    // Update is called once per frame
-    void Update()
+    private System.Collections.IEnumerator CicloSubirBajar()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(tiempoSubidaMin, tiempoSubidaMax));
+            SubirObjeto();
+
+            yield return new WaitForSeconds(Random.Range(tiempoMantenimientoMin, tiempoMantenimientoMax));
+
+            if (colisiona)
+            {
+                BajarObjeto();
+                colisiona = false; 
+            }
+            else
+            {
+                puntuacion++;
+                Debug.Log("Puntuación: " + puntuacion);
+            }
+        }
     }
+
+    private void SubirObjeto()
+    {
+        Debug.Log("Subiendo objeto");
+
+    }
+
+    private void BajarObjeto()
+    {
+        Debug.Log("Bajando objeto");
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        colisiona = true;
+    }
+
 }
