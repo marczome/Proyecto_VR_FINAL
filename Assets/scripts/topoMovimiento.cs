f3bce7466ad132824f57716730771a2d26b9f78e
marczome <marczm090102@gmail.com>
2024-01-06 19:12:24 +0100

    Modelos y materiales (parte_1)

--- Assets/scripts/topoMovimiento.cs
+++ Assets/scripts/topoMovimiento.cs
@@ -2,28 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class topoMovimiento : MonoBehaviour
 {
     public float tiempoSubidaMin = 1f;
     public float tiempoSubidaMax = 4f;
     public float tiempoMantenimientoMin = 1f;
     public float tiempoMantenimientoMax = 4f;
-    public float tiempoBajadaMin = 1f;
-    public float tiempoBajadaMax = 4f;
 
-    public float incrementoAltura = 0.5f; 
+    public float incrementoAltura = 0.5f;
     private float alturainicial;
-    
 
     public int puntuacion = 0;
-    
 
     private bool colisiona = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        colisiona = true;
+       
+    }
 
     private void Start()
     {
         alturainicial = transform.position.y;
         StartCoroutine(CicloSubirBajar());
+    }
+
+    private void SubirObjeto()
+    {
+        transform.position = new Vector3(transform.position.x, alturainicial + incrementoAltura, transform.position.z);
+        Debug.Log("Subiendo objeto");
+    }
+
+    private void BajarObjeto()
+    {
+        transform.position = new Vector3(transform.position.x, alturainicial, transform.position.z);
+        Debug.Log("Bajando objeto");
     }
 
     private IEnumerator CicloSubirBajar()
@@ -32,8 +47,6 @@ public class topoMovimiento : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(tiempoSubidaMin, tiempoSubidaMax));
             SubirObjeto();
-
-            yield return new WaitForSeconds(Random.Range(tiempoMantenimientoMin, tiempoMantenimientoMax));
 
             if (colisiona)
             {
@@ -44,28 +57,16 @@ public class topoMovimiento : MonoBehaviour
             }
             else
             {
+                yield return new WaitForSeconds(Random.Range(tiempoMantenimientoMin, tiempoMantenimientoMax));
                 BajarObjeto();
             }
         }
     }
-
-    private void SubirObjeto()
-    {
-        transform.position = new Vector3(transform.position.x, alturainicial + incrementoAltura, transform.position.z);
-        Debug.Log("Subiendo objeto");
-
-    }
-
-    private void BajarObjeto()
-    {
-        transform.position = new Vector3(transform.position.x, alturainicial, transform.position.z);
-        Debug.Log("Bajando objeto");
-
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        colisiona = true;
-    }
-
 }
+
+
+
+
+
+
+
